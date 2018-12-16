e447c6968aec7574baa56035343fc4b24c37cd53
Elizabeth Ramirez <eramirezvillamizar@gmail.com>
2018-12-16 16:04:34 -0500

    agrego vista mapa estrategico

--- front/public/js/charts-custom.js
+++ front/public/js/charts-custom.js
@@ -389,6 +389,99 @@ $(document).ready(function () {
         }
     });
 
+    // ------------------------------------------------------- //
+    // Bar Chart
+    // ------------------------------------------------------ //
+    var BARCHARTEXMPLE    = $('#barChartExample2');
+    var barChartExample2 = new Chart(BARCHARTEXMPLE, {
+        type: 'bar',
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }]
+            },
+        },
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Data Set 1",
+                    backgroundColor: [
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1
+                    ],
+                    hoverBackgroundColor: [
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1
+                    ],
+                    borderColor: [
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1
+                    ],
+                    borderWidth: 1,
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                },
+                {
+                    label: "Data Set 2",
+                    backgroundColor: [
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2
+                    ],
+                    hoverBackgroundColor: [
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2
+                    ],
+                    borderColor: [
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2
+                    ],
+                    borderWidth: 1,
+                    data: [35, 40, 60, 47, 88, 27, 30],
+                }
+            ]
+        }
+    });
+
 
 
     // ------------------------------------------------------- //
