fefc7cda7ffda925d897e75ddf7b2e59eed3720b
Elizabeth Ramirez <eramirezvillamizar@gmail.com>
2018-12-20 07:54:46 -0500

    q

--- front/js/elizajs.js
+++ front/js/elizajs.js
@@ -97,8 +97,8 @@ function pintarMenus(nombres,ids){
 								}
 							}else{
 								str+='<img class="card-img" src="'+Indicador.imagen+'" alt="Card image"></div><div class="col-sm-6">';
-							}						
-				
+							}
+
                 str+='<div class="containerJ">';
 								str+='<a class="btn btn-primaryJ" data-toggle="tooltip" href="javascript:preCargarDetalles(\''+Indicador.id+'\',\''+Indicador.nombre+'\',\'insert\')" data-placement="top" title="Detalles"><i class="material-icons">event_note</i></a>';
 								str+='<a class="btn btn-primaryJ usuarioAdmin" data-toggle="tooltip" href="javascript:editarIndicador(\''+Indicador.id+'\',\''+Indicador.nombre+'\')" data-placement="top" title="Editar"><i class="material-icons">create</i></a>';
@@ -259,6 +259,7 @@ $("#idDescripcion").val(string);
                     processData: false,
                     cache: false,
                     success: function (data) {
+											alert(data);
 											var json=JSON.parse(data);
                         if (json[0].msg== "exito") {
 														 	 //insertar periodo
